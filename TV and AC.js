9f2fda6cf5e51eddbb27aced9c80d3ae1ae0b408
prashee1107 <102855826+prashee1107@users.noreply.github.com>
2023-02-09 17:34:28 +0530

    Add files via upload

---
+++ TV and AC.js
@@ -0,0 +1,30 @@
+img = "";
+status = "";
+odject = [];
+
+function preload(){
+    img = loadImage("TV and AC.jpeg");
+}
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd",modelloaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function modelloaded(){
+    console.log("Model Loaded!");
+   status = "true";
+   objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        object = results;
+    }
+}
+function draw(){
+    image(img,0,0,640,420);
+}
